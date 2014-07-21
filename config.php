10f3e63acd039711bd242144b9479bc02d04a507
syifa <syifaifaaa@gmail.com>
2014-07-21 12:05:26 +0700

    udah diubah

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database_name = "db_pkl";
+$pdo = new PDO("mysql:host=$host;db_name=$database_name", $user, $pass, array(
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+	));
+
+?>
