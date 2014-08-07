ca5e3172da675418bcb63e741824e0fb0d435d7e
syifa <syifaifaaa@gmail.com>
2014-08-07 13:32:28 +0700

    layout login sementara

--- login.php
+++ login.php
@@ -1,34 +1,65 @@
 <html>
 	<head>
-	<title> Log In </title>
+	<title> Login Data Siswa PKL </title>
+	<!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 	</head>
 
 <body>
-<center>
-		<table>
-			<form action="proses/proses_login.php" method="POST">
-			<tr>
-			<td> Username </td>
-			<td> : </td>
-			<td>
-			<input type="text" name="user" placeholder="input your username ...">
-			</td>
-			</tr>
-			<tr>
-			<td> Password </td>
-			<td> : </td>
-			<td>
-			<input type="password" name="pass" placeholder="input your password ...">
-			</td>
-			</tr>
-			<tr>
-			<td>
-			<input type="submit" name="login" value="login">
-			</td>
-			</tr>
-			
-			</form>
-		</table>
-</center>
+<div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form" action="proses/proses_login.php" method="POST">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Username" name="user" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" name="pass" type="password" value="">
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+                                <input type="submit" name="login" value="Login" class="btn btn-lg btn-success btn-block">
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="js/plugins/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/sb-admin-2.js"></script>
 </body>
 </html>
+
+
