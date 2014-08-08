6625463e1a93655f8aa8683a443dec146067c3ba
abidahhss <a.abidah06@gmail.com>
2014-08-08 09:48:58 +0700

    pertaon

--- modul/laporan_pertahun/laporan_pertahun.php
+++ modul/laporan_pertahun/laporan_pertahun.php
@@ -7,6 +7,7 @@
 <tr>
 	<td> No </td>
 	<td> Nis </td>
+	<td> Nama </td>
 	<td> Sekolah/Universitas</td>
 	<td> Tanggal Masuk </td>
 	<td> Tanggal Keluar</td>
@@ -26,7 +27,7 @@ while ($laporan_taun = mysql_fetch_array($qry))
 	<td><?php echo $laporan_taun['tgl_masuk']?></td>
 	<td><?php echo $laporan_taun['tgl_keluar']?></td>
 	<td><?php echo $laporan_taun['periode_pkl']?></td>
-	<td><a href="index.php?pages=view_pertahun&nis=<?php echo $laporan_taun['nis'];?>"> Lihat </a>
+	<td><a href="index.php?pages=view_laporan&nis=<?php echo $laporan_taun['nis'];?>"> Lihat </a>
         </a>
     </td>
 </tr>
