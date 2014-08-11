41b1f44d0310019d36bb953698f431059e2f9225
syifa <syifaifaaa@gmail.com>
2014-08-11 09:50:35 +0700

    dashboard

--- modul/dashb.php
+++ modul/dashb.php
@@ -1,12 +1,149 @@
- <div class="col-lg-12">
-    <h1 class="page-header">
-        <h1>Selamat Datang Administrator !</h1>
-        <p>Selamat datang di Aplikasi Data Siswa PKL PT. Jerbee Indonesia</p>
-        <p>
-          <a class="btn btn-primary btn-large" href="index.php?pages=data_siswa">
-            Lanjutkan 
-          </a>
-        </p>
-    </h1>
-    <div class="col-md-10"></div>
+<br>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="jumbotron">
+            <h1>Selamat Datang Administrator !</h1>
+            <p>Selamat datang di Aplikasi Data Siswa PKL PT. Jerbee Indonesia</p>
+            <p><a class="btn btn-primary btn-lg" role="button" href="index.php?pages=list_siswa">Lanjutkan</a>
+            </p>
+        </div>
+    </div>
 </div>
+<div class="col-lg-4 col-md-8">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-xs-3">
+                    <i class="fa fa-user fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <div class="huge">26</div>
+                    <div>Data Siswa PKL</div>
+                </div>
+             </div>
+        </div>
+        <a href="index.php?pages=list_siswa">
+	        <div class="panel-footer">
+	            <span class="pull-left">Lihat Selengkapnya</span>
+	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+	            <div class="clearfix"></div>
+	        </div>
+        </a>
+    </div>
+</div>
+<div class="col-lg-4 col-md-8">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-xs-3">
+                    <i class="fa fa-home fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <div class="huge">26</div>
+                    <div>Data Sekolah</div>
+                </div>
+             </div>
+        </div>
+        <a href="index.php?pages=list_sekolah">
+	        <div class="panel-footer">
+	            <span class="pull-left">Lihat Selengkapnya</span>
+	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+	            <div class="clearfix"></div>
+	        </div>
+        </a>
+    </div>
+</div>
+<div class="col-lg-4 col-md-8">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-xs-3">
+                    <i class="fa fa-user fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <div class="huge">26</div>
+                    <div>Data Pembimbing</div>
+                </div>
+             </div>
+        </div>
+        <a href="index.php?pages=list_pembimbing">
+	        <div class="panel-footer">
+	            <span class="pull-left">Lihat Selengkapnya</span>
+	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+	            <div class="clearfix"></div>
+	        </div>
+        </a>
+    </div>
+</div>
+<div class="col-lg-4 col-md-8">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-xs-3">
+                    <i class="fa fa-book fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <div class="huge">26</div>
+                    <div>Data Program Keahlian</div>
+                </div>
+             </div>
+        </div>
+        <a href="index.php?pages=list_prog_keahlian">
+	        <div class="panel-footer">
+	            <span class="pull-left">Lihat Selengkapnya</span>
+	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+	            <div class="clearfix"></div>
+	        </div>
+        </a>
+    </div>
+</div>
+<div class="col-lg-4 col-md-8">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-xs-3">
+                    <i class="fa fa-user fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <div class="huge">26</div>
+                    <div>Data User</div>
+                </div>
+             </div>
+        </div>
+        <a href="index.php?pages=list_user">
+	        <div class="panel-footer">
+	            <span class="pull-left">Lihat Selengkapnya</span>
+	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+	            <div class="clearfix"></div>
+	        </div>
+        </a>
+    </div>
+</div>
+<div class="col-lg-4 col-md-8">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-xs-3">
+                    <i class="fa fa-file fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <div class="huge">26</div>
+                    <div>Laporan Pertahun</div>
+                </div>
+             </div>
+        </div>
+        <a href="index.php?pages=laporan_pertahun">
+	        <div class="panel-footer">
+	            <span class="pull-left">Lihat Selengkapnya</span>
+	            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+	            <div class="clearfix"></div>
+	        </div>
+        </a>
+    </div>
+</div>
+
+
+
+
+
+
