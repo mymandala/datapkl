2d4b140b2d06b82498e401a68e99eb8866d98683
safitri <rohendawatisafitri@gmail.com>
2014-08-13 11:14:11 +0700

    update view

--- modul/data_siswa/view.php
+++ modul/data_siswa/view.php
@@ -10,74 +10,82 @@ if ($_GET['pages']=='view') { ?>
 		while ($data = mysql_fetch_array($sql)) { ?>
 			<table>
 				<tr>
-					<td> NIS </td>
-					<td width="30"> : </td>
-					<td> <?php echo $data['nis']; ?> </td>
-				</tr>
-				<tr>
-					<td> Nama Siswa </td>
-					<td> : </td>
-					<td> <?php echo $data['nama_siswa']; ?> </td>
-				</tr>
-				<tr>
-					<td> Jenis Kelamin </td>
-					<td> : </td>
-					<td> <?php echo $data['jenis_kelamin']; ?> </td>
-				</tr>
-				<tr>
-					<td> Agama </td>
-					<td> : </td>
-					<td> <?php echo $data['agama']; ?> </td>
-				</tr>
-				<tr>
-					<td> Tempat Lahir </td>
-					<td> : </td>
-					<td> <?php echo $data['tempat_lahir']; ?> </td>
-				</tr>
-				<tr>
-					<td> Tanggal Lahir </td>
-					<td> : </td>
-					<td> <?php echo tgl_indo($data['tanggal_lahir']); ?> </td>
-				</tr>
-				<tr>
-					<td> Alamat </td>
-					<td> : </td>
-					<td> <?php echo $data['alamat']; ?> </td>
-				</tr>
-				<tr>
-					<td> Nama Sekolah/Universitas </td>
-					<td> : </td>
-					<td> <?php echo $data['s_nama']; ?> </td>
-				</tr>
-				<tr>
-					<td> Nama Pembimbing </td>
-					<td> : </td>
-					<td> <?php echo $data['p_nama']; ?> </td>
-				</tr>
-				<tr>
-					<td> Program Keahlian </td>
-					<td> : </td>
-					<td> <?php echo $data['nama_prog_keahlian']; ?> </td>
-				</tr>
-				<tr>
-					<td> Periode PKL </td>
-					<td> : </td>
-					<td> <?php echo $data['periode_pkl']; ?> </td>
-				</tr>
-				<tr>
-					<td> Tanggal Masuk </td>
-					<td> : </td>
-					<td> <?php echo tgl_indo($data['tgl_masuk']); ?> </td>
-				</tr>
-				<tr>
-					<td> Tanggal Keluar </td>
-					<td> : </td>
-					<td> <?php echo tgl_indo($data['tgl_keluar']); ?> </td>
-				</tr>
-				<tr>
-					<td> Photo </td>
-					<td> : </td>
-					<td> <img src="images/<?php echo $data['photo']; ?>" width="200" height="200" </td>
+					<td>
+						<table>
+							<tr>
+								<td> NIS </td>
+								<td width="30"> : </td>
+								<td> <?php echo $data['nis']; ?> </td>
+							</tr>
+							<tr>
+								<td> Nama Siswa </td>
+								<td> : </td>
+								<td> <?php echo $data['nama_siswa']; ?> </td>
+							</tr>
+							<tr>
+								<td> Jenis Kelamin </td>
+								<td> : </td>
+								<td> <?php echo $data['jenis_kelamin']; ?> </td>
+							</tr>
+							<tr>
+								<td> Agama </td>
+								<td> : </td>
+								<td> <?php echo $data['agama']; ?> </td>
+							</tr>
+							<tr>
+								<td> Tempat Lahir </td>
+								<td> : </td>
+								<td> <?php echo $data['tempat_lahir']; ?> </td>
+							</tr>
+							<tr>
+								<td> Tanggal Lahir </td>
+								<td> : </td>
+								<td> <?php echo tgl_indo($data['tanggal_lahir']); ?> </td>
+							</tr>
+							<tr>
+								<td> Alamat </td>
+								<td> : </td>
+								<td> <?php echo $data['alamat']; ?> </td>
+							</tr>
+							<tr>
+								<td> Nama Sekolah/Universitas </td>
+								<td> : </td>
+								<td> <?php echo $data['s_nama']; ?> </td>
+							</tr>
+							<tr>
+								<td> Nama Pembimbing </td>
+								<td> : </td>
+								<td> <?php echo $data['p_nama']; ?> </td>
+							</tr>
+							<tr>
+								<td> Program Keahlian </td>
+								<td> : </td>
+								<td> <?php echo $data['nama_prog_keahlian']; ?> </td>
+							</tr>
+							<tr>
+								<td> Periode PKL </td>
+								<td> : </td>
+								<td> <?php echo $data['periode_pkl']; ?> </td>
+							</tr>
+							<tr>
+								<td> Tanggal Masuk </td>
+								<td> : </td>
+								<td> <?php echo tgl_indo($data['tgl_masuk']); ?> </td>
+							</tr>
+							<tr>
+								<td> Tanggal Keluar </td>
+								<td> : </td>
+								<td> <?php echo tgl_indo($data['tgl_keluar']); ?> </td>
+							</tr>
+						</table>
+					<td width="200"></td>
+					<td>
+						<table>
+							<tr>
+								<td> <img src="images/<?php echo $data['photo']; ?>" width="200" height="200" ></td>
+							</tr>
+						</table>
+					</td>
 				</tr>
 			</table>
 		</div>
